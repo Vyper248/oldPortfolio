cc0153859a417bb193aafdb6b4353f1435e7f5d9
Vyper248 <vyper375@hotmail.com>
2018-10-15 17:24:07 +0100

    added error boundry to card list

--- CompleteWebDevCourse/first-react/src/containers/App.js
+++ CompleteWebDevCourse/first-react/src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -28,7 +29,9 @@ class App extends Component {
                     <h1 id='heading'>Robo Friends</h1>
                     <SearchBox searchfield={this.searchfield} searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
