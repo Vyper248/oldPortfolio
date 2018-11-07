a9bcda78891c0d5a8f22803659830ef91cc4fab5
Vyper248 <vyper375@hotmail.com>
2018-11-07 20:07:41 +0000

    styling. Added function to add links to coming soon page for all nav links without an href

--- CompleteWebDevCourse/LearnWithSarah/js/addTemplates.js
+++ CompleteWebDevCourse/LearnWithSarah/js/addTemplates.js
@@ -20,8 +20,19 @@ async function createNavbar() {
     const navbtn = document.querySelector('#navbar a[href="'+thisPage+'"]');
     if (navbtn) navbtn.classList.add('selected');
 
+    addComingSoonLinks();
     setupMenuEvents();
     setupLanguageBtns(thisPage, en);
+}
+
+function addComingSoonLinks(){
+    const menuBtns = document.querySelectorAll('.menu a.basicBtn');
+    Array.from(menuBtns).forEach(btn => {
+        const href = btn.getAttribute('href');
+        if (!href){
+            btn.setAttribute('href', 'comingSoon.html');
+        }
+    });
 }
 
 function setupMenuEvents(){
@@ -84,8 +95,8 @@ function setupMenuEvents(){
 
 function setupLanguageBtns(thisPage, en){
     //temp code while working on website - only add links for pages that exist
-    const finishedPagesEn = ['faq.html', 'index.html', 'contact.html', 'about.html'];
-    const finishedPagesFa = ['faq.html', 'index.html', 'about.html', 'contact.html'];
+    const finishedPagesEn = ['faq.html', 'index.html', 'contact.html', 'about.html', 'comingSoon.html'];
+    const finishedPagesFa = ['faq.html', 'index.html', 'about.html', 'contact.html', 'comingSoon.html'];
     
     if (en && finishedPagesFa.includes(thisPage)){
         const farsiBtn = document.querySelector('#farsiBtn');
